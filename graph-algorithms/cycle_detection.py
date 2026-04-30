22251eca3d5e33d3a64d2bcf7123469739dc3ab8
Jamalludeen <jamalghazniwal@gmail.com>
2026-04-30 16:20:39 +0430

    implement cycle detection for directed graphs

--- graph-algorithms/cycle_detection.py
+++ graph-algorithms/cycle_detection.py
@@ -21,6 +21,27 @@ def has_cycle_undirected(graph: Dict[Hashable, List[Hashable]]) -> bool:
     return False
 
 
+def has_cycle_directed(graph: Dict[Hashable, List[Hashable]]) -> bool:
+    WHITE, GRAY, BLACK = 0, 1, 2
+    color: Dict[Hashable, int] = {node: WHITE for node in graph}
+
+    def dfs(node: Hashable) -> bool:
+        color[node] = GRAY
+        for neighbor in graph[node]:
+            if color[neighbor] == GRAY:
+                return True
+            if color[neighbor] == WHITE and dfs(neighbor):
+                return True
+        color[node] = BLACK
+        return False
+
+    for node in graph:
+        if color[node] == WHITE:
+            if dfs(node):
+                return True
+    return False
+
+
 undirected_cyclic = {
     'A': ['B', 'C'],
     'B': ['A', 'C'],
@@ -33,6 +54,21 @@ undirected_acyclic = {
     'C': ['B'],
 }
 
+directed_cyclic = {
+    'A': ['B'],
+    'B': ['C'],
+    'C': ['A'],
+}
+
+directed_acyclic = {
+    'A': ['B', 'C'],
+    'B': ['D'],
+    'C': ['D'],
+    'D': [],
+}
+
 if __name__ == "__main__":
     print(has_cycle_undirected(undirected_cyclic))
     print(has_cycle_undirected(undirected_acyclic))
+    print(has_cycle_directed(directed_cyclic))
+    print(has_cycle_directed(directed_acyclic))
