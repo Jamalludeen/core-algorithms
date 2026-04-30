7a9b53e26df3aab5502cf5ba209469abfe2804eb
Jamalludeen <jamalghazniwal@gmail.com>
2026-04-30 16:19:55 +0430

    add shortest path by edge count using BFS

--- graph-algorithms/bfs.py
+++ graph-algorithms/bfs.py
@@ -1,5 +1,5 @@
 from collections import deque
-from typing import Dict, Hashable, List, Set
+from typing import Dict, Hashable, List, Optional, Set
 
 
 def bfs(graph: Dict[Hashable, List[Hashable]], start: Hashable) -> None:
@@ -15,6 +15,30 @@ def bfs(graph: Dict[Hashable, List[Hashable]], start: Hashable) -> None:
                 queue.append(neighbor)
 
 
+def shortest_path_bfs(
+    graph: Dict[Hashable, List[Hashable]],
+    start: Hashable,
+    goal: Hashable,
+) -> Optional[List[Hashable]]:
+    if start == goal:
+        return [start]
+
+    visited: Set[Hashable] = {start}
+    queue = deque([(start, [start])])
+
+    while queue:
+        node, path = queue.popleft()
+        for neighbor in graph[node]:
+            if neighbor in visited:
+                continue
+            new_path = path + [neighbor]
+            if neighbor == goal:
+                return new_path
+            visited.add(neighbor)
+            queue.append((neighbor, new_path))
+    return None
+
+
 graph = {
     'A': ['B', 'C'],
     'B': ['A', 'D', 'E'],
@@ -27,3 +51,4 @@ graph = {
 if __name__ == "__main__":
     bfs(graph, 'A')
     print()
+    print(shortest_path_bfs(graph, 'A', 'F'))
